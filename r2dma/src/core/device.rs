072bdb73ecd80c2b924e719c6e671a642d80632a
SF-Zhou <sfzhou.scut@gmail.com>
2024-12-15 23:17:53 +0800

    continue

--- r2dma/src/core/device.rs
+++ r2dma/src/core/device.rs
@@ -1,3 +1,4 @@
+use super::DeviceConfig;
 use crate::{ibv, Result};
 use std::borrow::Cow;
 
@@ -10,7 +11,7 @@ pub struct Device {
     // The index of device in list.
     index: DeviceIndex,
     /// The protection domain associated with the device.
-    _pd: ibv::ProtectionDomain,
+    pd: ibv::ProtectionDomain,
     /// The context associated with the device.
     context: ibv::Context,
     /// The attributes of the device,
@@ -29,7 +30,7 @@ pub struct Port {
 }
 
 impl Device {
-    pub fn open(index: DeviceIndex, device: &ibv::Device) -> Result<Self> {
+    pub fn open(index: DeviceIndex, device: &ibv::Device, config: &DeviceConfig) -> Result<Self> {
         let context = ibv::Context::create(device)?;
         let pd = ibv::ProtectionDomain::create(&context)?;
         let device_attr = context.query_device()?;
@@ -42,6 +43,20 @@ impl Device {
             for gid_index in 0..port_attr.gid_tbl_len as u16 {
                 if let Ok(gid) = context.query_gid(port_num, gid_index) {
                     let gid_type = context.query_gid_type(port_num, gid_index)?;
+                    if !config.gid_type_filter.is_empty()
+                        && !config.gid_type_filter.contains(&gid_type)
+                    {
+                        continue;
+                    }
+
+                    if config.roce_v2_skip_link_local_addr && gid_type == ibv::GidType::RoCEv2 {
+                        let ip = gid.as_ipv6();
+                        if (ip.segments()[0] & 0xffc0) == 0xfe80 {
+                            // unstable feature: ip.is_unicast_link_local()
+                            continue;
+                        }
+                    }
+
                     gids.push((gid_index, gid, gid_type))
                 }
             }
@@ -55,7 +70,7 @@ impl Device {
 
         Ok(Self {
             index,
-            _pd: pd,
+            pd,
             context,
             device_attr,
             ports,
@@ -66,8 +81,16 @@ impl Device {
         self.index
     }
 
+    pub fn pd(&self) -> &ibv::ProtectionDomain {
+        &self.pd
+    }
+
     pub fn context(&self) -> &ibv::Context {
         &self.context
+    }
+
+    pub fn guid(&self) -> u64 {
+        self.context.device().guid()
     }
 
     pub fn name(&self) -> Cow<str> {
@@ -91,7 +114,14 @@ mod tests {
     fn test_ib_device() {
         let device_list = ibv::DeviceList::available().unwrap();
         let first_device = device_list.first().unwrap();
-        let device = Device::open(DeviceIndex(0), first_device).unwrap();
+        let mut config = DeviceConfig::default();
+        let device = Device::open(DeviceIndex(0), first_device, &config).unwrap();
+        println!("{}: {:#?}", device.name(), device);
+        assert_eq!(device.index().0, 0);
+
+        config.gid_type_filter = [ibv::GidType::IB, ibv::GidType::RoCEv2].into();
+        config.roce_v2_skip_link_local_addr = true;
+        let device = Device::open(DeviceIndex(0), first_device, &config).unwrap();
         println!("{}: {:#?}", device.name(), device);
     }
 }
